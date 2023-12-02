d229a9a215880b0e4efa888e2d60da1feb7ec04d
kylin <kylinsoong.1214@gmail.com>
2023-12-02 10:22:15 +0800

    update changes

---
+++ pytorch/load_model.py
@@ -0,0 +1,25 @@
+import torch
+from torch import nn
+
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28*28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10),
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
+
+model = NeuralNetwork()
+
+model = torch.load('model.pth')
+print(model)
---
+++ pytorch/save_model.py
@@ -0,0 +1,7 @@
+import torch
+import torchvision.models as models
+
+model = models.vgg16(weights='IMAGENET1K_V1')
+torch.save(model.state_dict(), 'model_weights.pth')
+
+
